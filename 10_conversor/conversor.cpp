584b5ab934f33645e6e2c6add4b1acfc2eaa5cd4
Luis Alberto Lastres alcaraz <alberto@IMPRESORA.local>
2015-11-18 22:39:38 +0100

    I think is too late, but here im uploading hex conversor. Good nights.

--- 10_conversor/conversor.cpp
+++ 10_conversor/conversor.cpp
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
 
 	int main(){
         int valor;
-
+        int hexdecimal;
 
         printf("Introduce el numero que quieres convertir: ");
         scanf(" %d", &valor);
         
         printf("%d en Hexadecimal es: %X\n",valor, valor);
         printf("%d en Octal es: %o\n", valor, valor);
-        printf("%d en Binario es: %d\n", valor, valor);
+        printf("%d en Binario aun no lo sé, pero pronto lo sabré.\n", valor);
 
 
 
