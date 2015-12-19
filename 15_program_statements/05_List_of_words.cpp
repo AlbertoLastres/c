84f8cfb51d66eba327db15f4e25b35ade73ec1f2
albertolastres <albertok.mobile@gmail.com>
2015-12-19 16:23:07 +0100

    Finished 05_List_of_words with personal features.

--- 15_program_statements/05_List_of_words.cpp
+++ 15_program_statements/05_List_of_words.cpp
@@ -3,12 +3,17 @@
 	
 	const char *lista[] = {
 
-	    "pan",
-	    "tostadas",
-	    "bacon",
-	    "tortilla",
-	    "cipotes",
-	    "deja de comer ya"
+	    "Estás gordo no comas más.",
+	    "Helado de vainilla.",
+	    "Natillas.",
+	    "Una cuajada.",
+	    "Un mero.",
+	    "Entrecot.",
+	    "Huevos rotos.",
+	    "Ensalada Mixta.",
+	    "Un poco de pan.",
+	    "Un tente en pié.",
+	    "Unos donuts."
 
 	};
 
@@ -16,12 +21,25 @@
 
 	    int cuanto;
 
-	    printf("Cuánto quieres comer hoy?\n");
+	    printf("Cuánto quieres comer hoy?(1-10)\n");
 	    scanf("%i", &cuanto);
 
-		printf("%s.\n", lista[cuanto]);
+		if(cuanto<1){
+			printf("Por favor solo puedes introducir números del 1 al 10.\n");
+	    		return EXIT_FAILURE;
+			}
+		if(cuanto>10){
+		    printf("Por favor solo puedes introducir números del 1 al 10.\n");
+			return EXIT_FAILURE;
+				}
+			
+		for(int contador=0; contador<cuanto; contador++){
+		
+		    printf("%s.\n", lista[cuanto]);
+		    cuanto = cuanto - 1;
 
 
+			}
 
 
 	return EXIT_SUCCESS;
