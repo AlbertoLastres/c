7d4be64dc38f9044237b06f96c6d5cf2d485eb8e
albertolastres <albertok.mobile@gmail.com>
2015-12-22 16:55:56 +0100

    nskell.cpp as new skel for ncurses.

---
+++ nskel.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+
+	int main(int argc, const char **argv){
+	
+	initscr();
+
+
+
+	endwin(); 
+
+	return EXIT_SUCCESS;
+	//"g++ <archivo.cpp> -lnurses" para compilar
+
+}
