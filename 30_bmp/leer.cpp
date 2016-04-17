c50dd731d78636a24acfe4c3ff82a83e65358fbd
Albertolastres <Albertok.mobile@gmail.com>
2016-04-17 21:30:04 +0200

    ejercicio palabras

--- 30_bmp/leer.cpp
+++ 30_bmp/leer.cpp
@@ -4,40 +4,43 @@
 
 #define MAX 4294967295
 
-struct Datos {
-    int Tipo;
-    int Tamano;
-    int Reservado1;
-    int Reservado2;
-    int Datos;
-    int Cabecera;
-    int Anchura;
-    int Altura;
-    int Planos;
-    int TamanoPuntos;
-    int Compresion;
-    int Tamanoimagen;
-    int ResolucionHorizontal;
-    int ResolucionVertical;
-    int TamanoTablaColor;
-    int ContadorColores;
+struct BMP {
+    char Tipo[2];
+    int file_size;
+    int res1;
+    int img_offset;
+    int header_size;
+    int width;
+    int height;
+    short bitplanes;
+    short point_size;
+    int compression;
+    int image_size;
+    int horz_size;
+    int vert_size;
+    int palette_size;
+    int important_colors;
+
 };
 
 
 	int main(int argc, const char **argv){
 
             FILE *pf = NULL;
+            struct BMP cabecera[54];
+            //struct Datos Estructura;
 
-
-            pf = fopen(argv[1], "r");
+            pf = fopen(argv[1], "r+");
 
             if (!pf){
                 printf("No puedo leer el archivo %s\n", argv[1]);
                 return EXIT_FAILURE;
             }
 
+            fread(&cabecera, sizeof(struct BMP), 1, pf);
             
-
+            for(int i=0; i<54; i++)
+                printf("%x ", &cabecera[i]);
 
             fclose(pf);
 	return EXIT_SUCCESS;
